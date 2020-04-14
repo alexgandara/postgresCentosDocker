2620775476de8df7191670b1698eb8657de04f11
Alejandro Gandara <30574592+alexgandara@users.noreply.github.com>
2020-04-14 15:04:04 -0500

    ***

--- dbmaster.sh
+++ dbmaster.sh
@@ -1,2 +1,2 @@
-docker run -p 5432:5432 --name dbmaster -v /Users/gandara/opt/postgresCentosDocker/data:/var/lib/postgresql/data -d -e POSTGRES_PASSWORD=SysMgr21720 --network="pgnetwork" gandara/postgres_debian:01
+docker run -p 5432:5432 --name dbmaster -v /Users/gandara/opt/postgresCentosDocker/data:/var/lib/postgresql/data -d -e POSTGRES_PASSWORD=******* --network="pgnetwork" gandara/postgres_debian:01
 

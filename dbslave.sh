6d588a1929591f1a6518b7d2e9c6e564f6bcf8bc
Alejandro Gandara <30574592+alexgandara@users.noreply.github.com>
2020-04-14 15:04:43 -0500

    ***

--- dbslave.sh
+++ dbslave.sh
@@ -1,2 +1,2 @@
-docker run -p 5431:5432 --name dbslave -v /Users/gandara/opt/postgresCentosDocker/slave_data:/var/lib/postgresql/data -d -e POSTGRES_PASSWORD=SysMgr21720 --network="pgnetwork" gandara/postgres_debian:01
+docker run -p 5431:5432 --name dbslave -v /Users/gandara/opt/postgresCentosDocker/slave_data:/var/lib/postgresql/data -d -e POSTGRES_PASSWORD=**** --network="pgnetwork" gandara/postgres_debian:01
 
